fdbb80cb9a33a504e42e5fc1ca9db22f3a181f54
Md Sarfaraz <112682898+sarfu786@users.noreply.github.com>
2023-04-17 09:28:26 +0530

    Update Weather.js

--- Weather.js
+++ Weather.js
@@ -10,7 +10,7 @@ let search=async()=>{
     
   }
   else{
-    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=1cb6532aea3c298a830a71380eace21e`
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=1cb6532aea3c298a830a71380eace21e`
     let response= await fetch(url)
     let info = await response.json()
     temperature.textContent = info.main.temp
